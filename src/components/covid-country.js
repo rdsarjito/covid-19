20348751e09c246adbfe58d1f27afb787b857f1e
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2020-05-04 20:09:52 +0700

    belum rapih

---
+++ src/components/covid-country.js
@@ -0,0 +1,13 @@
+class CovidCountry extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `<select name="" class="selectCountry">
+        <option selected disabled>Select Country</option>
+        </select>`
+    }
+}
+
+customElements.define("covid-country", CovidCountry);
