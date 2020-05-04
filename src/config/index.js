e7714c6e9c25275e1ba749e1b1b68fde798e0997
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-05-04 20:47:42 +0700

    config url

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://covid19.mathdro.id/';
+
+const ENDPOINT_COUNTRIES = `${BASE_URL}api/countries`;
+const ENDPOINT_CONFIRMED = `${BASE_URL}api/confirmed`;
+const ENDPOINT_SUMMARY = `${BASE_URL}/api/`;
