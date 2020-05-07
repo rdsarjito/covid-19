29f615db4056ccf2b07ae5c9ac27db36b6ea31a3
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-05-07 22:27:40 +0700

    split css bundle

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,16 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
  
  
 module.exports = merge(common, {
     mode: "production",
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             /* babel loader */
